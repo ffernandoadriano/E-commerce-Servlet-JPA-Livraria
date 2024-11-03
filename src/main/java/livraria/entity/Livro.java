0627631ff77fc0e61eb76e954d41cbbc39666c5a
ffernandoadriano <lfernando.adriano@gmail.com>
2024-11-03 20:34:05 -0300

    Criando as classes Livro, Pedido e Usuario

---
+++ src/main/java/livraria/entity/Livro.java
@@ -0,0 +1,85 @@
+package livraria.entity;
+
+import java.util.Objects;
+
+public class Livro {
+
+	private Integer id;
+	private String titulo;
+	private String autor;
+	private String editora;
+	private Integer ano;
+	private Double preco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", editora=" + editora + ", ano=" + ano
+				+ ", preco=" + preco + "]";
+	}
+
+}
---
+++ src/main/java/livraria/entity/Pedido.java
@@ -0,0 +1,88 @@
+package livraria.entity;
+
+import java.sql.Date;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Pedido {
+
+	private String id;
+	private Date data;
+	private Integer status;
+	private Usuario usuario;
+	private Double valor;
+	private Set<Livro> livros = new LinkedHashSet<>();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Set<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(Set<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(data, id, status);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(data, other.data) && Objects.equals(id, other.id) && Objects.equals(status, other.status);
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", data=" + data + ", status=" + status + ", usuario=" + usuario + ", valor="
+				+ valor + ", livros=" + livros + "]";
+	}
+
+}
