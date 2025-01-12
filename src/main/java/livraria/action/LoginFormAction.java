7dc4f59c85d5687705b1c4f0d70dca0762bbd9ae
Fernando Adriano <lfernando.adriano@gmail.com>
2025-01-12 19:05:03 -0300

    adicionando o LoginFormAction

---
+++ src/main/java/livraria/action/LoginFormAction.java
@@ -0,0 +1,16 @@
+package livraria.action;
+
+import webf.action.Action;
+
+/**
+ * Abre a tela de formulário de login
+ */
+public class LoginFormAction extends Action {
+
+	@Override
+	public void process() throws Exception {
+		getSession().setAttribute("menuAtivo", "login");
+		forward("login.jsp");
+	}
+
+}
